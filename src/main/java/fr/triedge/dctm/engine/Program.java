5e0ee6bd52141959d19b4b4b1b524df3de0b8d6c
biehlst1 <biehlst1@CHALMW-D03352.europe.actelion.com>
2018-11-28 15:15:07 +0100

    Include libs

--- src/main/java/fr/triedge/dctm/engine/Program.java
+++ src/main/java/fr/triedge/dctm/engine/Program.java
@@ -1,9 +1,32 @@
 package fr.triedge.dctm.engine;
 
-public class Program {
+import fr.triedge.dctm.ui.MainWindow;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Program extends Application{
+	
+	private MainWindow mainWindow;
 
 	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		initProgram();
+	}
+
+	private void initProgram() {
 		
+	}
+
+	public MainWindow getMainWindow() {
+		return mainWindow;
+	}
+
+	public void setMainWindow(MainWindow mainWindow) {
+		this.mainWindow = mainWindow;
 	}
 
 }
---
+++ src/main/java/fr/triedge/dctm/ui/MainWindow.java
@@ -0,0 +1,26 @@
+package fr.triedge.dctm.ui;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainWindow {
+
+	private Stage stage;
+	private Scene scene;
+
+	public Stage getStage() {
+		return stage;
+	}
+
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
+
+	public Scene getScene() {
+		return scene;
+	}
+
+	public void setScene(Scene scene) {
+		this.scene = scene;
+	}
+}
